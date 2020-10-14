577bb61d3942ff1d3d1277bcb1efe3efb91cbce0
Valeriy Makhnovets <lerunchicm@gmail.com>
2020-10-14 22:28:37 +0300

    fix creators of List

--- src/main/java/homework_3/task_1/cooks/Chef.java
+++ src/main/java/homework_3/task_1/cooks/Chef.java
@@ -7,6 +7,7 @@ import homework_3.task_1.dish.Salad;
 import homework_3.task_1.products.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Chef {
@@ -14,11 +15,11 @@ public class Chef {
     public static void main(String[] args) {
         System.out.println("Chef start work");
         List<Ingredient> ingredientsInRefrigerator = createIngredientInRefrigerator();
-        List<Ingredient> ingredientsForBorscht=createIngredientListForBorscht();
-        List<Ingredient> ingredientsForSalad=createIngredientListForSalad();
-        Dish borscht=new Borscht(ingredientsForBorscht);
+        List<Ingredient> ingredientsForBorscht = createIngredientListForBorscht();
+        List<Ingredient> ingredientsForSalad = createIngredientListForSalad();
+        Dish borscht = new Borscht(ingredientsForBorscht);
         Dish salad = new Salad(ingredientsForSalad);
-        DinnerWithTwoDish dinner = new DinnerWithTwoDish(salad,borscht);
+        DinnerWithTwoDish dinner = new DinnerWithTwoDish(salad, borscht);
         System.out.println("---------------------------");
         System.out.println("Chef make a dinner");
         dinner.cook();
@@ -32,48 +33,32 @@ public class Chef {
         System.out.println("---------------------------");
         System.out.println("Chef sort vegetables in borscht from dinner");
         List<Ingredient> getIngredientsByFilter =
-                borscht.getIngredientsByFilter(30,50,100,150);
+                borscht.getIngredientsByFilter(30, 50, 100, 150);
         printIngredientList(getIngredientsByFilter);
 
     }
 
-    public static List<Ingredient> createIngredientInRefrigerator(){
+    public static List<Ingredient> createIngredientInRefrigerator() {
         System.out.println("Create ingredients in refrigerator");
-        List<Ingredient> ingredients = new ArrayList<>();
-        ingredients.add(new Beef(500));
-        ingredients.add(new Beet(2300));
-        ingredients.add(new Potatoes(1000));
-        ingredients.add(new Onion(500));
-        ingredients.add(new Cabbage(2000));
-        ingredients.add(new Carrot(800));
-        ingredients.add(new Tomato(1500));
-        return ingredients;
+        return Arrays.asList(new Beef(500), new Beet(2300),
+                new Potatoes(1000), new Onion(500), new Cabbage(2000),
+                new Carrot(800), new Tomato(1500));
     }
 
-    public static List<Ingredient> createIngredientListForBorscht (){
+    public static List<Ingredient> createIngredientListForBorscht() {
         System.out.println("Get ingredients for borscht");
-        List<Ingredient> ingredients = new ArrayList<>();
-        ingredients.add(new Beef(500));
-        ingredients.add(new Beet(330));
-        ingredients.add(new Potatoes(350));
-        ingredients.add(new Onion(130));
-        ingredients.add(new Cabbage(300));
-        ingredients.add(new Carrot(130));
-        return ingredients;
+        return Arrays.asList(new Beef(500), new Beet(330), new Potatoes(350),
+                new Onion(130), new Cabbage(300), new Carrot(130));
     }
 
-    public static List<Ingredient> createIngredientListForSalad (){
+    public static List<Ingredient> createIngredientListForSalad() {
         System.out.println("Get ingredients for salad");
-        List<Ingredient> ingredients = new ArrayList<>();
-        ingredients.add(new Beef(100));
-        ingredients.add(new Cabbage(150));
-        ingredients.add(new Carrot(250));
-        ingredients.add(new Tomato(300));
-        return ingredients;
+        return Arrays.asList(new Beef(100), new Cabbage(150),
+                new Carrot(250), new Tomato(300));
     }
 
-    public static void printIngredientList(List<Ingredient> ingredients){
-        for (Ingredient ingredient: ingredients) {
+    public static void printIngredientList(List<Ingredient> ingredients) {
+        for (Ingredient ingredient : ingredients) {
             System.out.println(ingredient.toString());
         }
     }
